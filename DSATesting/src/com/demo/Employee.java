48bd657043d93234623de8d31082738a8fdef67a
Admin <Admin@LAPTOP-K9QFL5GN>
2024-04-02 15:45:36 +0530

    Parag

---
+++ DSATesting/src/com/demo/Employee.java
@@ -0,0 +1,20 @@
+package com.demo;
+
+public class Employee 
+{
+	int id;
+	String name;
+	 public Employee(int id,String name)
+	 {
+		 this.id=id;
+		 this.name=name;
+	 }
+	public static void main(String[] args) 
+	{
+		Employee e=new Employee(100,"Savani");
+		System.out.println(e.id);
+		System.out.println(e.name);
+
+	}
+
+}
